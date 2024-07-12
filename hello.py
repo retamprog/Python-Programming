6e4342cd772bfe12fa1e01b19b00732a2c9163d5
retamprog <120020649+retamprog@users.noreply.github.com>
2024-07-12 14:01:30 +0530

    second commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('this is my python program')
+var='hello '*10
+# the variable var has stored the python expression
+print(var)
+# prints the string hello 10 times  -- ('hello '*10) is a python expression
+# it first evaluates the expression then prints it
+# the python interpreter interprets the python code line by line
+# since unlike java it does not having the system of semicolons which denote the end of  a line of code
+# therefore here we cannot write two different codes at the same line
+# we are now going to learn about python variables
+is_present=True
+# python is case-sensitive we must type keywords with the correct case otherwise the interpreter won't recognise it
